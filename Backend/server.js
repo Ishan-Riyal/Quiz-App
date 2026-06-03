959c85261aed220e1278e394593581cc1e95015a
Ishan Riyal <ishanriyal07@gmail.com>
2026-06-04 03:12:31 +0530

    Add vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -17,9 +17,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:8173",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   }),
 );
